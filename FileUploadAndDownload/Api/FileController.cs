8655aeb3bec57bd7af0bef2f296c399335ebee18
bert <136035200@qq.com>
2019-03-31 16:41:18 +0800

    formdata传参

--- FileUploadAndDownload/Api/FileController.cs
+++ FileUploadAndDownload/Api/FileController.cs
@@ -1,4 +1,5 @@
 ﻿using FileUploadAndDownload.Provider;
+using Newtonsoft.Json;
 using System;
 using System.Collections.Generic;
 using System.IO;
@@ -13,6 +14,11 @@ namespace FileUploadAndDownload.Api
 {
     public class FileController : ApiController
     {
+        private JsonSerializerSettings _camelCaseSetting = new JsonSerializerSettings
+        {
+            ContractResolver = new Newtonsoft.Json.Serialization.CamelCasePropertyNamesContractResolver()
+            //, NullValueHandling = NullValueHandling.Ignore
+        };
         /// <summary>
         /// 文件上传
         /// </summary>
@@ -20,7 +26,7 @@ namespace FileUploadAndDownload.Api
         /// <returns></returns>
         [HttpPost]
         [Route("api/Upload")]
-        public async Task<string> Post(string guid)
+        public async Task<IHttpActionResult> Post(string guid)
         {
 
             if (!Request.Content.IsMimeMultipartContent())
@@ -33,13 +39,36 @@ namespace FileUploadAndDownload.Api
 
             List<string> files = new List<string>();
             var provider = new CustomMultipartFormDataStreamProvider(uploadFolderPath, guid);
+            var res = string.Empty;
             try
             {
                 // Read the form data.
                 await Request.Content.ReadAsMultipartAsync(provider);
 
-                // This illustrates how to get the file names.
 
+                string param1 = string.Empty;
+                string param2 = string.Empty;
+                //// This illustrates how to get the form data.
+                foreach (var key in provider.FormData.AllKeys)
+                {
+                    if (key == "param1") param1 = provider.FormData.GetValues(key)[0];
+                    if (key == "param2") param2 = provider.FormData.GetValues(key)[0];
+                }
+                if (string.IsNullOrEmpty(param1))
+                {
+                    res=HttpStatusCode.BadRequest + ",no param1";
+                    return Json(res, _camelCaseSetting);
+                }
+                if (string.IsNullOrEmpty(param2))
+                {
+                    res = HttpStatusCode.BadRequest + ",no param2";
+                    return Json(res, _camelCaseSetting);
+                }
+                if (provider.FileData == null || provider.FileData.Count == 0)
+                {
+                    return Json(HttpStatusCode.BadRequest + ", no file", _camelCaseSetting);
+                }
+                // This illustrates how to get the file names.
                 foreach (var file in provider.FileData)
                 {//接收文件
                     files.Add(Path.GetFileName(file.LocalFileName));
@@ -49,7 +78,8 @@ namespace FileUploadAndDownload.Api
             {
                 throw;
             }
-            return string.Join(",", files);
+            return Json(string.Join(",", files), _camelCaseSetting);
         }
+        //方法二https://www.cnblogs.com/GarsonZhang/p/5511427.html
     }
 }
